ee76aec8e4eb78610f0d20f482bf8aff69698362
Hiumathy Lam <Hiumathy@Hiumathys-MacBook-Pro.local>
2016-10-20 15:24:53 -0500

    BinarySearchRecursive

--- main.c
+++ main.c
@@ -1,62 +1,9 @@
-#include <stdlib.h>
+//
+//  main.c
+//  Practice
+//
+//  Created by Hiumathy Lam on 10/20/16.
+//  Copyright © 2016 Hiumathy Lam. All rights reserved.
+//
+
 #include <stdio.h>
-
-struct node {
-    int value;
-    struct node *next;
-};
-struct head {
-    int length;
-    struct node * first;
-};
-
-struct head * headNode;
-struct node * temp;
-
-struct node * CreateNode(int val){
-    struct node * link = (struct node*)malloc(sizeof(struct node));
-    link->value = val;
-    link->next = NULL;
-    return link;
-}
-
-void push(int val){
-    struct node * link = CreateNode(val);
-    link->next = headNode->first;
-    headNode->first = link;
-    headNode->length +=1;
-}
-void pop(){
-    temp = headNode->first;
-    headNode->first = temp->next;
-    free(temp);
-    headNode->length -= 1;
-}
-
-void Print(){
-    temp = headNode->first;
-    while (temp != NULL){
-        printf("%d ", temp->value);
-        temp = temp->next;
-    }
-    printf("\n");
-}
-
-
-int main(){
-    headNode = (struct head*)malloc( sizeof(struct head));
-    int length = 0;
-    headNode->first = NULL;
-    headNode->length = length;
-
-    push(10);
-    Print();
-    push(40);
-    Print();
-    push(50);
-    Print();
-    push(20);
-    Print();
-    pop();
-    Print();
-}
